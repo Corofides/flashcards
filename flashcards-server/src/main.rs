a9876fa4e63036553ff231a4f35eb78e889dfc8f
corofides <jamlendrem@gmail.com>
2026-01-29 20:46:45 +0000

    Init commit for Flashcard Project for Feb

---
+++ flashcards-server/src/main.rs
@@ -0,0 +1,59 @@
+use axum::{
+    routing::get,
+    Router,
+    response::Json,
+};
+use serde_json::{Value, json};
+use serde::Serialize;
+
+#[tokio::main]
+async fn main() {
+
+    let app = Router::new()
+        .route("/health", get(get_health))
+        .route("/cards", get(get_cards));
+
+    //let app = Router::new().route("/health", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    println!("Hello, world!");
+}
+
+
+#[derive(Default, Serialize)]
+struct Card {
+    id: usize,
+    front: String,
+    back: String,
+}
+
+impl Card {
+    fn new(id: usize, front: String, back: String) -> Self {
+        Card {
+            id,
+            front,
+            back,
+        }
+    }
+}
+
+async fn get_cards() -> Json<Value> {
+    let mut cards = vec![];
+
+    cards.push(Card::new(0, String::from("Ballet Flats"), String::new()));
+    cards.push(Card::new(1, String::from("Pumps"), String::new()));
+    cards.push(Card::new(2, String::from("Loafers"), String::new()));
+
+    Json(json!(
+        cards
+    ))
+}
+
+async fn get_health() -> String {
+    String::from("200 OK")
+    /* Json(json!(
+        {"state": String::from("running")}
+    )) */
+}
