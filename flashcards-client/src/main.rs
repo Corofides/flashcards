4878f9d6ff7cf7bdce701a2adddd81b8aaf59fe2
corofides <jamlendrem@gmail.com>
2026-02-05 19:33:40 +0000

    Clean the file;

--- flashcards-client/src/main.rs
+++ flashcards-client/src/main.rs
@@ -102,19 +102,11 @@ fn Content() -> HtmlResult {
             let mut cards = (*cards).clone();
 
             if let Some(card) = cards.get_mut(*card_index) {
-                //let mut card = card.clone();
                 card.flip_card();
             }
 
             handle.set(Some(Rc::new(cards)));
 
-            //let mut new_cards = (*cards).clone();
-
-            /* if let Some(card) = cards.get_mut(*card_index) {
-                (*card).flip_card();
-            } */
-
-            // cards.set(new_cards);
         }
     };
 
@@ -148,93 +140,13 @@ fn Content() -> HtmlResult {
 #[component]
 fn App() -> Html {
 
-    /*let cards = use_state(|| {
-        let vec: Vec<CardState> = Vec::new();
-        vec
-    });
-    {
-        let cards = cards.clone();
-        use_effect_with((), move |_| {
-            let cards = cards.clone();
-            wasm_bindgen_futures::spawn_local(async move {
-                let fetched_cards: Vec<Card> = Request::get("http://localhost:3000/cards")
-                    .send()
-                    .await
-                    .unwrap()
-                    .json()
-                    .await
-                    .unwrap();
-                    
-                let fetched_cards: Vec<CardState> = fetched_cards.iter()
-                    .map(move |card| CardState::new(card.clone()))
-                    .collect();
-
-                cards.set(fetched_cards);
-            });
-        });
-    }
-    
-    let card_index = use_state(|| 0);
-    let total_cards = cards.len();
-
-    let next_card = {
-        let card_index = card_index.clone();
-        move |_| {
-            let value = (*card_index + 1) % total_cards;
-            card_index.set(value);
-        }
-    };
-
-    let flip_card = {
-        let cards = cards.clone();
-        let card_index = card_index.clone();
-        //let current_card = current_card.clone();
-        move |_| {
-
-            let mut new_cards = (*cards).clone();
-
-            if let Some(card) = new_cards.get_mut(*card_index) {
-                card.flip_card();
-            }
-
-            cards.set(new_cards);
-        }
-    };
-
-    let prev_card = {
-        let card_index = card_index.clone();
-        move |_| {
-
-            let value = if *card_index == 0 {
-                total_cards.saturating_sub(1)
-            } else {
-                *card_index - 1
-            };
-
-            card_index.set(value);
-        }
-    };
-
-    let card = cards.get(*card_index).clone(); */
     let fallback = html! {<div>{ "Loading..."}</div>};
 
-    //if let Some(card) = card {
     html! {
         <Suspense {fallback}>
-            /* <div>
-                <CardDiv card={card.unwrap().clone()} />
-                <button onclick={prev_card}>{ "Prev Card" }</button>
-                <button onclick={flip_card}>{ "Turn Card" }</button>
-                <button onclick={next_card}>{ "Next Card" }</button>
-            </div> */
             <Content />
         </Suspense>
     }
-    /*} else {
-        html! {
-            <div>{ "Loading..." }</div>
-        }
-    }*/
 }
 
 fn main() {
