d4f367341e773ceb06b21814531cfc64cfb97896
corofides <jamlendrem@gmail.com>
2026-02-08 08:46:31 +0000

    Add a post request for creating a new card;

--- flashcards-server/src/main.rs
+++ flashcards-server/src/main.rs
@@ -5,12 +5,22 @@ use http::header::HeaderValue;
 use http::Method;
 use axum::{
     extract::State,
-    routing::get,
+    routing::{
+        get,
+        post,
+    },
     Router,
     response::Json,
 };
+use serde::{Deserialize};
 use serde_json::{Value, json};
 use std::sync::{Arc, Mutex};
+
+#[derive(Deserialize)]
+struct CreateCardPayload {
+    front: String,
+    back: String,
+}
 
 struct AppState {
     cards: Mutex<Vec<Card>>,
@@ -48,11 +58,28 @@ async fn main() {
     let app = Router::new()
         .route("/health", get(get_health))
         .route("/cards", get(get_cards))
+        .route("/cards", post(add_card))
         .with_state(shared_state)
         .layer(cors);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+
+}
+
+async fn add_card(State(state): State<Arc<AppState>>, Json(payload): Json<CreateCardPayload>) -> String {
+
+    let cards = &mut state.cards.lock().unwrap();
+
+    let cards_total = cards.len();
+
+    cards.push(Card::new(
+        cards_total,
+        payload.front.clone(),
+        payload.back.clone(),
+    ));
+
+    format!("Card Added! {} total cards!", cards_total + 1)
 
 }
 
