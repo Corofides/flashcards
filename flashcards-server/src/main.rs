e0a27f4f821caea40fbff8acea307b7a8139ccb4
corofides <jamlendrem@gmail.com>
2026-02-08 07:58:07 +0000

    Update server to use state

--- flashcards-server/src/main.rs
+++ flashcards-server/src/main.rs
@@ -4,14 +4,42 @@ use tower_http::cors::{CorsLayer};
 use http::header::HeaderValue;
 use http::Method;
 use axum::{
+    extract::State,
     routing::get,
     Router,
     response::Json,
 };
 use serde_json::{Value, json};
+use std::sync::{Arc, Mutex};
+
+struct AppState {
+    cards: Mutex<Vec<Card>>,
+}
 
 #[tokio::main]
 async fn main() {
+
+    let cards = vec![
+        Card::new(
+            0, 
+            String::from("Simple, slip on shoes with very thin soles and no heel"), 
+            String::from("Ballet Flats")
+        ),
+        Card::new(
+            1,
+            String::from("The quintessential heeled shoe. They are closed toe and usually have a seamless, low cut front. They don't have straps or laces - you just slide your foot in."),
+            String::from("Pumps (or Court Shoes)"),
+        ),
+        Card::new(
+            2,
+            String::from("A more structured, masculine inspired slip-on shoe. They often have a slightly thicker sole than a ballet flat, and a distinct tongue that covers more of the top of the foot"),
+            String::from("Loafers"),
+        ),
+    ];
+
+    let shared_state = Arc::new(AppState {
+        cards: Mutex::new(cards),
+    });
 
     let cors = CorsLayer::new()
         .allow_origin("http://localhost:8080".parse::<HeaderValue>().unwrap())
@@ -20,6 +48,7 @@ async fn main() {
     let app = Router::new()
         .route("/health", get(get_health))
         .route("/cards", get(get_cards))
+        .with_state(shared_state)
         .layer(cors);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -27,15 +56,11 @@ async fn main() {
 
 }
 
-async fn get_cards() -> Json<Value> {
-    let mut cards = vec![];
-
-    cards.push(Card::new(0, String::from("Ballet Flats"), String::new()));
-    cards.push(Card::new(1, String::from("Pumps"), String::new()));
-    cards.push(Card::new(2, String::from("Loafers"), String::new()));
+async fn get_cards(State(state): State<Arc<AppState>>) -> Json<Value> {
+    let cards = state.cards.lock().unwrap();
 
     Json(json!(
-        cards
+        *cards
     ))
 }
 
